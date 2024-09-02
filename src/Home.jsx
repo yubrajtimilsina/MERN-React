6457b3e761dae2f020af0f18dc40b438cab3fcef
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2024-09-02 21:06:54 +0545

    add

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,32 @@
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 function Home(){
-    return(
-        <h1>Haha</h1>
+
+    return (
+       <>
+        <nav className="navbar">
+        <div className="container">
+            <a href="#" class="brand">My Website</a>
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><Link to="/about">About</Link></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+
+ 
+    <div className="main-content">
+        <div className="container">
+            <h1>Welcome to My Website</h1>
+            <p>This is a simple page with a navbar and some content in the middle.</p>
+            <p>You can customize this template to fit your needs. Happy coding!</p>
+        </div>
+    </div>
+       </>
     )
 }
+
 export default Home
